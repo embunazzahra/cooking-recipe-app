40bfed9d975bb7134bc9b58ce82bfd1e8a27f257
embunazzahra <dhauembunazzahra@gmail.com>
2022-06-12 11:58:18 +0700

    android init

---
+++ android/Cooking Recipe App/app/src/main/java/com/embun/cookingrecipeapp/ui/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.embun.cookingrecipeapp.ui;
+
+import com.embun.cookingrecipeapp.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
